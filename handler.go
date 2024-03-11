d5f7da4fd56e681eeb8ab13c2c990651cf21bbf3
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-11 16:09:02 +0800

    Update handler.go

---
+++ handler.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+    "log"
+    "net"
+    "net/http"
+
+    "golang.org/x/net/webdav"
+    "github.com/elazarl/goproxy"
+    "github.com/elazarl/goproxy/ext/auth"
+)
+
+func webdavHandler(dir, prefix string) http.Handler {
+    return &webdav.Handler{
+        FileSystem: webdav.Dir(dir),
+        Prefix:     prefix,
+        LockSystem: webdav.NewMemLS(),
+    }
+}
+
+func proxyHandler(hostname, username, password string) (http.Handler, error) {
+    proxy := goproxy.NewProxyHttpServer()
+    auth.ProxyBasic(proxy, hostname, func(usr, pwd string) bool {
+        return usr == username && pwd == password
+    })
+    proxy.NonproxyHandler = http.HandlerFunc(ipDisplayHandler)
+    return proxy, nil
+}
+
+func ipDisplayHandler(w http.ResponseWriter, r *http.Request) {
+    clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
+    if err != nil {
+        log.Printf("[WARN] %v", err)
+        return
+    }
+    if _, err := w.Write([]byte(clientIP + "\n")); err != nil {
+        log.Printf("[WARN] %v", err)
+        return
+    }
+}
