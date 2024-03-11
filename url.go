59c38cec68c282b11ceb7da23361fe05850e7037
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-11 16:18:20 +0800

    Update url.go

---
+++ url.go
@@ -0,0 +1,41 @@
+package url
+
+import (
+    "net/url"
+)
+
+type ParsedURL struct {
+    Scheme   string
+    Username string
+    Password string
+    Hostname string
+    Port     string
+    Path     string
+    Fragment string
+}
+
+func urlParse(rawURL string) (ParsedURL, error) {
+    u, err := url.Parse(rawURL)
+    if err != nil {
+        return ParsedURL{}, err
+    }
+    username := ""
+    password := ""
+    if u.User != nil {
+        username = u.User.Username()
+        password, _ = u.User.Password()
+    }
+    port := ""
+    if u.Port() != "" {
+        port = u.Port()
+    }
+    return ParsedURL{
+        Scheme:   u.Scheme,
+        Username: username,
+        Password: password,
+        Hostname: u.Hostname(),
+        Port:     port,
+        Path:     u.Path,
+        Fragment: u.Fragment,
+    }, nil
+}
