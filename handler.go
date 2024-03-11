9de96e88acd189de999c912111b10ba8d2e1affd
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-11 22:09:46 +0800

    Update handler.go

--- handler.go
+++ handler.go
@@ -10,7 +10,7 @@ import (
     "github.com/elazarl/goproxy/ext/auth"
 )
 
-func webdavHandler(dir, prefix string) http.Handler {
+func WebdavHandler(dir, prefix string) http.Handler {
     return &webdav.Handler{
         FileSystem: webdav.Dir(dir),
         Prefix:     prefix,
@@ -18,16 +18,16 @@ func webdavHandler(dir, prefix string) http.Handler {
     }
 }
 
-func proxyHandler(hostname, username, password string) http.Handler {
+func ProxyHandler(hostname, username, password string) http.Handler {
     proxy := goproxy.NewProxyHttpServer()
     auth.ProxyBasic(proxy, hostname, func(usr, pwd string) bool {
         return usr == username && pwd == password
     })
-    proxy.NonproxyHandler = http.HandlerFunc(ipDisplayHandler)
+    proxy.NonproxyHandler = http.HandlerFunc(IPDisplayHandler)
     return proxy
 }
 
-func ipDisplayHandler(w http.ResponseWriter, r *http.Request) {
+func IPDisplayHandler(w http.ResponseWriter, r *http.Request) {
     clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
     if err != nil {
         log.Printf("[WARN] %v", err)
