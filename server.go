e118df6eb2809c0b19130fc85ed9ecf2b82ad106
raymondragon <54016243+raymondragon@users.noreply.github.com>
2024-03-15 09:13:05 +0800

    Update server.go

--- server.go
+++ server.go
@@ -6,6 +6,10 @@ import (
     "net/http"
 )
 
+func ServeHTTP(hostname, port string, handler http.Handler) error {
+    return http.ListenAndServe(net.JoinHostPort(hostname, port), handler)
+}
+
 func ServeHTTPS(hostname, port string, handler http.Handler, tlsConfig *tls.Config) error {
     server := &http.Server{
         Addr:      net.JoinHostPort(hostname, port),
